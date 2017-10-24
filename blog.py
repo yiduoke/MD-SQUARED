cbb53060adb070408596cd86766d8f47af646b61
doniatung <doniatung32@gmail.com>
2017-10-23 22:51:54 -0400

    trying to organize basic structure

--- blog.py
+++ blog.py
@@ -4,37 +4,33 @@ import os
 my_app = Flask(__name__)
 my_app.secret_key = os.urandom(32)
 
-login = {"hello":"life"}
 
 @my_app.route("/", methods = ['GET','POST'])
 def root():
-    print session
-    if "login" in session:
-        return redirect(url_for('response'))
-    else:
-        return render_template('login.html', loggingOut = False, error = False, errorStatement = "")
+    session["login"] = True
+    session["username"] = request.form["username"]
+    session["password"] = request.form["password"]
+    return render_template ('home.html', username = session["username"]) 
+    
 
-@my_app.route("/response", methods = ['GET','POST'])
-def response():
-    for username in login:
-        if (request.form["username"] == username):
-            if(request.form["password"] == login[username]):
-                session["login"] = True
-                session["username"] = request.form["username"]
-                session["password"] = request.form["password"]
-                return render_template('welcome.html', username = session["username"]) 
-            else:
-                return render_template('login.html', loggingOut = False, error = True, errorStatement = "Invalid password.")
-        else:
-            return render_template('login.html', loggingOut = False, error = True, errorStatement = "Invalid username.")
 
-        
+@my_app.route("/search",methods = ['GET', 'POST'])
+def search():
+    pass
+
+@my_app.route("/newpost",methods=['POST'])
+def newPost():
+    pass
+
+@my_app.route("/editpost",methods=['GET', 'POST'])
+def editPost():
+    pass
+
 @my_app.route("/logout", methods = ['GET', 'POST'])
 def logout():
     session.clear()
     #removes all the session details
     return redirect(url_for('root'))
-    #return render_template('form.html', loggingOut = True, error = False, errorStatement = "")
 
 if __name__ == '__main__':
     my_app.debug = True
