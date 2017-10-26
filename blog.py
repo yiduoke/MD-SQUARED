4c0dcfba317b7a292dab042fc4a74eb707d7b2b8
Yiduo Ke <yiduoke822@jhs185.org>
2017-10-26 13:48:50 -0400

    yay signup works

--- blog.py
+++ blog.py
@@ -1,6 +1,54 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-from utils import db
+import sqlite3
 import os
+
+f = "databases.db"
+
+db = sqlite3.connect(f)
+c = db.cursor()
+
+c.execute('CREATE TABLE IF NOT EXISTS creds(username TEXT, pass TEXT);')
+c.execute('CREATE TABLE IF NOT EXISTS userblog(username TEXT, blogname TEXT, id INTEGER);')
+c.execute('CREATE TABLE IF NOT EXISTS blog(entry TEXT, id INTEGER);')
+
+def updateCreds(user, passw):
+    db = sqlite3.connect(f)
+    c = db.cursor()
+
+    c.execute('INSERT INTO creds VALUES(?,?)', [user, passw])
+
+    db.commit()
+    db.close()
+
+def checkLogin(user, passw):
+    db = sqlite3.connect(f)
+    c = db.cursor()
+
+    c.execute('SELECT EXISTS(SELECT 1 FROM creds WHERE username==? AND pass==? LIMIT 1);', [user, passw])
+
+    db.commit()
+    db.close()
+
+blogUpdater = 'UPDATE blog SET entry += %s WHERE id = %s'
+
+def updateBlog(newEntry, idEntry):
+    db = sqlite3.connect(f)
+    c = db.cursor()
+
+    c.execute(blogUpdater % (newEntry, idEntry))
+
+    db.commit()
+    db.close()
+
+def updateUserBlog(user, name, idEntry):
+    db = sqlite3.connect(f)
+    c = db.cursor()
+
+    c.execute('INSERT INTO userblog VALUES(?,?,?)' [user, name, idEntry])
+    
+    db.commit()
+    db.close()
+#---------------------------------------------------------------------------------------------------------------------
 
 my_app = Flask(__name__)
 my_app.secret_key = os.urandom(32)
@@ -8,15 +56,16 @@ my_app.secret_key = os.urandom(32)
 @my_app.route("/", methods = ['GET','POST'])
 def root():
     if (session.has_key("username")):
-        session["login"] = True
-        session["username"] = request.form["username"]
-        session["password"] = request.form["password"]
         return render_template('home.html', username = session["username"], loggedIn = True, blogcontent = "something from the datatable")
     else:
         return render_template('home.html')
 
 @my_app.route("/login", methods = ['GET', 'POST'])
 def login():
+    
+    # session["login"] = True
+    # session["username"] = request.form["username"]
+    # session["password"] = request.form["password"]
     return render_template('login.html')
 
 @my_app.route("/signup", methods = ['GET','POST'])
@@ -26,7 +75,7 @@ def signup():
 @my_app.route("/signedUp",methods = ["GET","POST"])
 def signedUp():
     # the following line cause an error. idk why. but if you comment them out the page loads
-    db.updateCreds(request.form["username"], request.form["password"])
+    updateCreds(request.form["username"], request.form["password"])
     return render_template("home.html");
 
 @my_app.route("/search",methods = ['GET', 'POST'])
@@ -51,3 +100,7 @@ if __name__ == '__main__':
     my_app.debug = True
     my_app.run()
 
+#------------------------------------------------------------------------------------------------------------------
+
+db.commit()
+db.close()
